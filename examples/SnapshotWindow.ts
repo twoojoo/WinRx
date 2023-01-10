033d24d2c8e3265a62bcab017d6a810532fbdcbf
twoojoo <twoojoo95@gmail.com>
2023-01-10 19:28:45 +0100

    new example folders

--- examples/SnapshotWindow.ts
+++
@@ -1,9 +0,0 @@
-import { snapshotWindow } from "../src"
-import { interval, tap } from "rxjs"
-
-interval(1).pipe(
-    snapshotWindow({offset: 5000, tolerance: 1}),
-    tap((v: number[]) => console.log("window closed -", v.length, "items"))
-).subscribe((v) => {
-    if (v.length != 1) throw Error(`expected 1 item, received ${v.length} items`)
-})
