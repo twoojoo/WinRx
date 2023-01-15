36a330ed3072d5d6268c334210e29e2efc528ed7
twoojoo <twoojoo95@gmail.com>
2023-01-15 11:54:53 +0100

    added mergeMap operator

---
+++ examples/mergeMap.ts
@@ -0,0 +1,34 @@
+import { Stream } from "../src/stream";
+import { EventEmitter } from "events"
+
+const emitter = new EventEmitter()
+
+const stream2 = Stream("stream2")
+    .fromEvent<string>(emitter, "stream2")
+    .map(e => e.value)
+
+const stream1 = Stream("stream1")
+    .fromEvent<number>(emitter, "stream1")
+    .map(e => e.value)
+    .mergeMap(
+        stream2,
+        e1 => e1 * 10,
+        e2 => parseInt(e2) * 100
+    )
+
+stream1.toEvent(emitter, "test-result")
+
+let counter1 = 0
+setInterval(() => {
+    emitter.emit("stream1", counter1)
+    counter1 += 2
+}, 500)
+
+let counter2 = ""
+setInterval(() => {
+    emitter.emit("stream2", counter2)
+    counter2 += "1"
+}, 250)
+
+
+emitter.on("test-result", (v) => console.log(v))
--- src/operators/merge.ts
+++ src/operators/merge.ts
@@ -2,8 +2,15 @@ import { streamFromSubject, subjectFromStream } from "../stream"
 import { Stream } from "../stream";
 import { Subject } from "rxjs"
 
+type Mapper<T, R> = (event: T) => Promise<R> | R
+
 export type Merge<T> = {
     merge: <R>(stream: Stream<R>) => Stream<T | R>
+    mergeMap: <R, N, M>(
+        stream: Stream<R>, 
+        mapper1: Mapper<T, N>, 
+        mapper2: Mapper<R, M>, 
+    ) => Stream<N | M>
 }
 
 export function mergeFactory<T>(source: Stream<T>): Merge<T> {
@@ -24,6 +31,27 @@ export function mergeFactory<T>(source: Stream<T>): Merge<T> {
             })
 
             return streamFromSubject(subj)
-        }
+        },
+        mergeMap<R, N, M>(
+            stream: Stream<R>, 
+            mapper1: Mapper<T, N>, 
+            mapper2: Mapper<R, M>
+        ): Stream<N | M> {
+            const subj = new Subject<N | M>()
+
+            subjectFromStream(source).subscribe({
+                async next(event: T) {
+                    subj.next(await mapper1(event))
+                }
+            })
+
+            subjectFromStream(stream).subscribe({
+                async next(event: R) {
+                    subj.next(await mapper2(event))
+                }
+            })
+
+            return streamFromSubject(subj)
+        },
     }
 }
