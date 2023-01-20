5c0780b7a6be6b3c704b8eed0c4071d2613d62f7
twoojoo <twoojoo95@gmail.com>
2023-01-20 19:13:30 +0100

    sources as operators

---
+++ src/operators/sources.ts
@@ -0,0 +1,73 @@
+import { Consumer, ConsumerConfig } from "kafkajs";
+import { EventEmitter } from "events"
+import { Stream, StreamContext, streamFromSubject } from "../stream";
+import { makeMetaEvent, MetaEvent } from "../event";
+import { Subject } from "rxjs";
+
+type KafkaEvent<E> = {
+    key: string,
+    value: E
+}
+
+type EmitterEvent<E> = {
+    name: string,
+    value: E
+}
+
+export type Sources = {   
+    /**Create a stream of kafka messages consumed from a topic or more (uses stream name as topic name if topics array is not provided)*/
+    fromKafka: <E>(consumer: Consumer, topics?: string[], config?: ConsumerConfig) => Stream<KafkaEvent<E>>
+    /**Create a stream from a named event (uses stream name as event name if not provided)*/
+    fromEvent: <E>(emitter: EventEmitter, name?: string) => Stream<EmitterEvent<E>>
+}
+
+export function sourcesFactory(ctx: StreamContext): Sources {
+    return {
+        fromKafka<E>(consumer: Consumer, topics?: string[], config?: ConsumerConfig): Stream<KafkaEvent<E>> {
+            if (!topics || topics.length == 0) topics = [ctx.name] //use stream name as topic if topics not provided
+
+            const sub = new Subject<MetaEvent<KafkaEvent<E>>>();
+            consumer.run({
+                ...config,
+                eachMessage: async ({ message, topic }) => {
+                    if (!topics.includes(topic)) return
+
+                    const event = {
+                        key: message.key.toString("utf-8"),
+                        value: attemptJsonParsing(message.value.toString("utf-8"))
+                    }
+
+                    await ctx.stateManager.enqueueEvent(makeMetaEvent(event))
+                    ctx.stateManager.dequeueLoop(sub)
+                }
+            })
+
+            return streamFromSubject(ctx, sub)
+        },
+
+        fromEvent<E>(emitter: EventEmitter, name?: string): Stream<EmitterEvent<E>> {
+            if (!name) name = ctx.name //use stream name as event name if event name is not provided
+
+            const sub = new Subject<MetaEvent<EmitterEvent<E>>>();
+            emitter.on(name, async (value) => {
+                const event = {
+                    name,
+                    value: attemptJsonParsing(value)
+                }
+
+                await ctx.stateManager.enqueueEvent(makeMetaEvent(event))
+                ctx.stateManager.dequeueLoop(sub)
+            })
+
+            return streamFromSubject(ctx, sub)
+        }
+    }
+}
+
+function attemptJsonParsing(value: string): any {
+    try {
+        return JSON.parse(value)
+    } catch (err) {
+        return value
+    }
+}
--- src/stream.ts
+++ src/stream.ts
@@ -1,5 +1,3 @@
-import { Consumer, ConsumerConfig } from "kafkajs";
-import { EventEmitter } from "events"
 import { Subject } from "rxjs";
 // import { Join, joinFactory } from "./operators/join"
 import { Merge, mergeFactory } from "./operators/merge"
@@ -8,13 +6,15 @@ import { Sinks, sinksFactory } from "./operators/sinks"
 // import { Windows, windowsFactory } from "./operators/windows"
 import { streamPool } from "./pool";
 import { StateManager } from "./state/state-manager";
-import { MetaEvent, makeMetaEvent } from "./event";
+import { MetaEvent } from "./event";
 import { Init, initFactory } from "./operators/init";
 import { MemoryStateManager } from "./state/Memory";
+import { sourcesFactory, Sources } from "./operators/sources";
 
 export type StreamContext = {
-    name: string,
+    name: string, //must be uniqued (checked at startup)
     stateManager: StateManager<any>
+    windows: string[] //collects stream windows names that must be unique per stream (checked at startup)
 }
 
 export type Stream<E> =
@@ -27,78 +27,16 @@ export type Stream<E> =
     { name: () => string } &
     { ctx: StreamContext }
 
-type KafkaEvent<E> = {
-    key: string,
-    value: E
-}
-
-type EmitterEvent<E> = {
-    name: string,
-    value: E
-}
-
-export type Sources = {   
-    /**Create a stream of kafka messages consumed from a topic or more (uses stream name as topic name if topics array is not provided)*/
-    fromKafka: <E>(consumer: Consumer, topics?: string[], config?: ConsumerConfig) => Stream<KafkaEvent<E>>
-    /**Create a stream from a named event (uses stream name as event name if not provided)*/
-    fromEvent: <E>(emitter: EventEmitter, name?: string) => Stream<EmitterEvent<E>>
-}
 
 /**Create a stream. Use the stream name as unique ideintifier. If a state manager is not provided, the state will be persited in memory.*/
 export function Stream(name: string, stateManager: StateManager<any> = new MemoryStateManager()): Sources {
-    if (!!streamPool[name]) throw Error(`a stream already exists with name "${name}"`)
+    if (!!streamPool[name]) throw Error(`a stream named "${name}" already exists in the stream pool`)
 
+    //setup stream context
     stateManager.setStreamName(name)
-    const ctx: StreamContext = { name, stateManager }
+    const ctx: StreamContext = { name, stateManager, windows: [] }
 
-    return {
-        fromKafka<E>(consumer: Consumer, topics?: string[], config?: ConsumerConfig): Stream<KafkaEvent<E>> {
-            if (!topics || topics.length == 0) topics = [ctx.name] //use stream name as topic if topics not provided
-
-            const sub = new Subject<MetaEvent<KafkaEvent<E>>>();
-            consumer.run({
-                ...config,
-                eachMessage: async ({ message, topic }) => {
-                    if (!topics.includes(topic)) return
-
-                    const event = {
-                        key: message.key.toString("utf-8"),
-                        value: attemptJsonParsing(message.value.toString("utf-8"))
-                    }
-
-                    await ctx.stateManager.enqueueEvent(makeMetaEvent(event))
-                    ctx.stateManager.dequeueLoop(sub)
-                }
-            })
-
-            return streamFromSubject(ctx, sub)
-        },
-
-        fromEvent<E>(emitter: EventEmitter, name?: string): Stream<EmitterEvent<E>> {
-            if (!name) name = ctx.name //use stream name as event name if event name is not provided
-
-            const sub = new Subject<MetaEvent<EmitterEvent<E>>>();
-            emitter.on(name, async (value) => {
-                const event = {
-                    name,
-                    value: attemptJsonParsing(value)
-                }
-
-                await ctx.stateManager.enqueueEvent(makeMetaEvent(event))
-                ctx.stateManager.dequeueLoop(sub)
-            })
-
-            return streamFromSubject(ctx, sub)
-        }
-    }
-}
-
-function attemptJsonParsing(value: string): any {
-    try {
-        return JSON.parse(value)
-    } catch (err) {
-        return value
-    }
+    return sourcesFactory(ctx)
 }
 
 /** Converts an RXJS Subject into a Stream object */
